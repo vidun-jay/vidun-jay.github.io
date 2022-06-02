00d8c5abbbe9531f7e256ce389ce10b815d856e6
vidun-jay <vidunvj@gmail.com>
2022-06-02 13:07:34 -0400

    Added new job

--- forms/contact.php
+++ forms/contact.php
@@ -17,20 +17,21 @@
 
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
-  
+
   $contact->to = $receiving_email_address;
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-
+  /*
   $contact->smtp = array(
-    'host' => 'smtp.gmail.com',
-    'username' => 'vidun.textalert@gmail.com',
-    'password' => 'ekkoxjokfwkowcla',
+    'host' => 'example.com',
+    'username' => 'example',
+    'password' => 'pass',
     'port' => '587'
   );
+  */
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
